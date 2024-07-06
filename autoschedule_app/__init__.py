132bb44e65d66c7a93956a1c6671e2bcf34ebe72
Audrine Whrite <117525099+nooby6@users.noreply.github.com>
2024-07-06 16:32:30 +0300

    installing flask logins to __init__.py

--- autoschedule_app/__init__.py
+++ autoschedule_app/__init__.py
@@ -1,17 +1,25 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
 
     db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'auth.login'
 
     with app.app_context():
         from .routes import main
         app.register_blueprint(main)
+
+        from .auth import auth as auth_blueprint
+        app.register_blueprint(auth_blueprint)
+
         db.create_all()
 
     return app
