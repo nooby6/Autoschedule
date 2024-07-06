c6db91f9bdabf4569608f2991d9db5f42852302f
Audrine Whrite <117525099+nooby6@users.noreply.github.com>
2024-07-06 16:21:09 +0300

    update to run.py

--- run.py
+++ run.py
@@ -1,4 +1,10 @@
+import os
+import sys
+print("Current working directory:", os.getcwd())
+print("Python path:", sys.path)
 
+# Add project directory to Python path
+sys.path.append(os.getcwd())
 
 from autoschedule_app import create_app
 
